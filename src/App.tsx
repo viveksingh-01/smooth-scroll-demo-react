b4273e0134bf57facf3592dd8c6e90cdedc4803b
Vivek Singh <viveksingh9301@gmail.com>
2026-06-14 12:28:33 +0530

    Enhance App component layout with a grid of image assets for improved visual showcase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,139 @@
+import type { CSSProperties } from "react";
+
 function App() {
   return (
-    <main>
-      <h2>Smooth Scroll Demo</h2>
+    <main className="bg-amber-50">
+      <section className="grid grid-cols-8 grid-rows-20 gap-2 overflow-hidden">
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 1, "--c": 2 } as CSSProperties}>
+          <img src="/assets/img/21.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 1, "--c": 5 } as CSSProperties}>
+          <img src="/assets/img/29.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 1, "--c": 8 } as CSSProperties}>
+          <img src="/assets/img/39.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 2, "--c": 3 } as CSSProperties}>
+          <img src="/assets/img/23.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 2, "--c": 7 } as CSSProperties}>
+          <img src="/assets/img/24.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 3, "--c": 1 } as CSSProperties}>
+          <img src="/assets/img/35.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 3, "--c": 4 } as CSSProperties}>
+          <img src="/assets/img/25.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 3, "--c": 8 } as CSSProperties}>
+          <img src="/assets/img/26.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 4, "--c": 2 } as CSSProperties}>
+          <img src="/assets/img/27.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 4, "--c": 5 } as CSSProperties}>
+          <img src="/assets/img/28.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 5, "--c": 3 } as CSSProperties}>
+          <img src="/assets/img/29.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 5, "--c": 8 } as CSSProperties}>
+          <img src="/assets/img/30.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 6, "--c": 2 } as CSSProperties}>
+          <img src="/assets/img/31.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 6, "--c": 7 } as CSSProperties}>
+          <img src="/assets/img/32.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 7, "--c": 4 } as CSSProperties}>
+          <img src="/assets/img/33.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 7, "--c": 6 } as CSSProperties}>
+          <img src="/assets/img/34.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 8, "--c": 3 } as CSSProperties}>
+          <img src="/assets/img/5.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 8, "--c": 8 } as CSSProperties}>
+          <img src="/assets/img/36.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 9, "--c": 1 } as CSSProperties}>
+          <img src="/assets/img/37.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 9, "--c": 6 } as CSSProperties}>
+          <img src="/assets/img/38.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 10, "--c": 3 } as CSSProperties}>
+          <img src="/assets/img/39.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 10, "--c": 8 } as CSSProperties}>
+          <img src="/assets/img/40.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 11, "--c": 2 } as CSSProperties}>
+          <img src="/assets/img/21.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 11, "--c": 7 } as CSSProperties}>
+          <img src="/assets/img/22.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 12, "--c": 1 } as CSSProperties}>
+          <img src="/assets/img/23.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 12, "--c": 5 } as CSSProperties}>
+          <img src="/assets/img/24.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 13, "--c": 2 } as CSSProperties}>
+          <img src="/assets/img/25.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 13, "--c": 7 } as CSSProperties}>
+          <img src="/assets/img/26.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 14, "--c": 3 } as CSSProperties}>
+          <img src="/assets/img/27.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 14, "--c": 6 } as CSSProperties}>
+          <img src="/assets/img/28.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 15, "--c": 4 } as CSSProperties}>
+          <img src="/assets/img/29.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 15, "--c": 8 } as CSSProperties}>
+          <img src="/assets/img/30.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 16, "--c": 4 } as CSSProperties}>
+          <img src="/assets/img/31.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 16, "--c": 7 } as CSSProperties}>
+          <img src="/assets/img/32.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 16, "--c": 1 } as CSSProperties}>
+          <img src="/assets/img/33.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 17, "--c": 8 } as CSSProperties}>
+          <img src="/assets/img/34.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 17, "--c": 3 } as CSSProperties}>
+          <img src="/assets/img/35.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 18, "--c": 7 } as CSSProperties}>
+          <img src="/assets/img/36.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 18, "--c": 4 } as CSSProperties}>
+          <img src="/assets/img/37.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 19, "--c": 8 } as CSSProperties}>
+          <img src="/assets/img/38.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 19, "--c": 2 } as CSSProperties}>
+          <img src="/assets/img/39.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 20, "--c": 6 } as CSSProperties}>
+          <img src="/assets/img/20.webp" />
+        </div>
+        <div className="grid__item shadow-md col-span-1 row-span-1" style={{ "--r": 20, "--c": 3 } as CSSProperties}>
+          <img src="/assets/img/1.webp" />
+        </div>
+      </section>
     </main>
   );
 }
