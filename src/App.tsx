6131d5732a9c5f12cfc889935cb70a2e538a4476
Vivek Singh <viveksingh9301@gmail.com>
2026-06-16 20:48:12 +0530

    Refactor App component by replacing Image components with Gallery component

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,9 @@
-import Image from "./components/Image";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <main className="bg-amber-50">
-      <section className="grid grid-cols-8 grid-rows-20 gap-2 overflow-hidden">
-        <Image src="21.webp" row={1} col={2} />
-        <Image src="29.webp" row={1} col={5} />
-        <Image src="39.webp" row={1} col={8} />
-        <Image src="23.webp" row={2} col={3} />
-        <Image src="24.webp" row={2} col={7} />
-        <Image src="35.webp" row={3} col={1} />
-        <Image src="25.webp" row={3} col={4} />
-        <Image src="26.webp" row={3} col={8} />
-        <Image src="27.webp" row={4} col={2} />
-        <Image src="28.webp" row={4} col={5} />
-        <Image src="29.webp" row={5} col={3} />
-        <Image src="30.webp" row={5} col={8} />
-        <Image src="31.webp" row={6} col={2} />
-        <Image src="32.webp" row={6} col={7} />
-        <Image src="33.webp" row={7} col={4} />
-        <Image src="34.webp" row={7} col={6} />
-        <Image src="5.webp" row={8} col={3} />
-        <Image src="36.webp" row={8} col={8} />
-        <Image src="37.webp" row={9} col={1} />
-        <Image src="38.webp" row={9} col={6} />
-        <Image src="39.webp" row={10} col={3} />
-        <Image src="40.webp" row={10} col={8} />
-        <Image src="21.webp" row={11} col={2} />
-        <Image src="22.webp" row={11} col={7} />
-        <Image src="23.webp" row={12} col={1} />
-        <Image src="24.webp" row={12} col={5} />
-        <Image src="25.webp" row={13} col={2} />
-        <Image src="26.webp" row={13} col={7} />
-        <Image src="27.webp" row={14} col={3} />
-        <Image src="28.webp" row={14} col={6} />
-        <Image src="29.webp" row={15} col={4} />
-        <Image src="30.webp" row={15} col={8} />
-        <Image src="31.webp" row={16} col={4} />
-        <Image src="32.webp" row={16} col={7} />
-        <Image src="33.webp" row={16} col={1} />
-        <Image src="34.webp" row={17} col={8} />
-        <Image src="35.webp" row={17} col={3} />
-        <Image src="36.webp" row={18} col={4} />
-        <Image src="37.webp" row={18} col={7} />
-        <Image src="38.webp" row={19} col={2} />
-        <Image src="39.webp" row={16} col={8} />
-        <Image src="20.webp" row={20} col={6} />
-        <Image src="1.webp" row={20} col={3} />
-      </section>
+      <Gallery />
     </main>
   );
 }
